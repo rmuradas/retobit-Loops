cee3841a59cddfa8144535e20a97364703362d92
Ruben Muradas <rmuradas@gmail.com>
2026-05-29 17:31:05 -0300

    mi solucion

--- src/main/java/Loops.java
+++ src/main/java/Loops.java
@@ -1,4 +1,5 @@
 public class Loops {
+
     /**
      * Reto 1: Suma de elementos con while
      * Usa un bucle while para recorrer el array "numbers" y sumar todos sus elementos.
@@ -9,9 +10,18 @@ public class Loops {
      */
     public static int sumArrayWhile() {
         int[] numbers = {4, 6, 9, 5};
+        int sum = 0;
+        int contador = 0;
+        while (contador < numbers.length) {
+            //System.out.println("El contador vale: " + contador);
+            sum = sum + numbers[contador];
+            // 3. ¡IMPORTANTE! Incrementamos el contador para no crear un bucle infinito
+            contador++;
+        }
+
         // Tu código aquí (usa while)
         // consejo: declara la variable "sum" fuera del loop
-        return 0; // Sustituye el 0 por la variable sum
+        return sum; // Sustituye el 0 por la variable sum
     }
 
     /**
@@ -24,9 +34,19 @@ public class Loops {
      */
     public static int countEvenNumbersDoWhile() {
         int[] numbers = {4, 6, 9, 5, 8};
+        int count = 0;
         // Tu código aquí (usa do while)
+        int contador = 0;
 
-        return 0; // Sustituye el 0 por la variable count
+        do {
+            //System.out.println("El contador vale: " + contador);
+            if (numbers[contador] % 2 == 0)
+                count = count + 1;
+            contador++; // Incrementamos
+        } while (contador < numbers.length); // 2. ...y "mientras" esto sea verdadero, repetir.
+
+        //System.out.println("¡Bucle terminado!");
+        return count; // Sustituye el 0 por la variable count
     }
 
     /**
@@ -39,10 +59,17 @@ public class Loops {
      */
     public static int findMaxWithFor() {
         int[] numbers = {4, 6, 9, 5, 3, 2};
+        int max = 0;
+
         // Tu código aquí (usa for)
         // consejo: declara la variable "max" fuera del loop
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
 
-        return 0; // Sustituye el 0 por la variable max
+        return max; // Sustituye el 0 por la variable max
     }
 
     public static void main(String[] args) {
